d54d4c98ac181fefed5ae7c20f4d8ac0f3436789
hjb0907 <hjb0907@naver.com>
2024-03-12 13:39:03 +0900

    오류 찾기

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,5 +1,10 @@
 import "reflect-metadata"
-import { DataSource } from "typeorm"
+import { DataSource } from "typeorm";
+import User from "./entities/User";
+import Post from "./entities/Post";
+import Sub from "./entities/Sub";
+import Comment from "./entities/Comment";
+import Vote from "./entities/Vote";
 
 export const AppDataSource = new DataSource({
     type: "postgres",
